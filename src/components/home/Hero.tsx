f3d253902cf809421f57a1daf0bd7e8772dfc158
kiwi-basu <sunishs9913@gmail.com>
2026-05-05 00:48:15 +0530

    fix : hero section registration closed for non payment user

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -6,7 +6,7 @@ import dateImage from "../../assets/Home/DateImage.png"
 
 const Hero = () => {
   // const user = JSON.parse(localStorage.getItem("user") || "null");
-  const { user } = useAuth();
+  const { user , hasPurchased } = useAuth();
 
   return (
     <section id="hero">
@@ -161,7 +161,7 @@ const Hero = () => {
 
         {/* buttons */}
         <div className="flex flex-col sm:flex-row gap-4 md:gap-5 z-10 w-full sm:w-auto">
-          {user ? (
+          {user && hasPurchased ? (
             // <Link to="/apply/payment" className="w-full sm:w-auto">
             //   <button className="w-full cursor-pointer px-8 md:px-20 py-3 text-lg sm:text-xl md:text-2xl rounded-full font-medium text-[#1F3A5F] bg-linear-to-r from-[#D8B893] via-[#CEAC81] to-[#BFA06F] shadow-md hover:shadow-lg hover:scale-105 transition-all duration-300">
             //     Register Now
@@ -172,6 +172,10 @@ const Hero = () => {
                 Select Companies
               </button>
             </Link>
+          ) : user && !hasPurchased ? (
+            <button className="w-full cursor-pointer px-8 md:px-20 py-3 text-lg sm:text-xl md:text-2xl rounded-full font-medium text-[#1F3A5F] bg-linear-to-r from-[#D8B893] via-[#CEAC81] to-[#BFA06F] shadow-md hover:shadow-lg hover:scale-105 transition-all duration-300">
+              Registration Closed    
+            </button>
           ) : (
             <>
               <Link to="/auth" className="w-full sm:w-auto">
